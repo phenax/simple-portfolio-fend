bb7f5c88c0447554f00144ea5ba0525adf96ab52
Akshay Nair <phenax5@gmail.com>
2016-11-24 16:57:46 +0530

    Added image optimization with gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp= require('gulp');
+
+const imageMin= require('gulp-imagemin');
+const imageResize= require('gulp-image-resize');
+const rename= require('gulp-rename');
+
+
+const BUILD_DIR= './src/img-min';
+const FILE_DIR= './src/img';
+
+const imageList= [
+	{
+		name: 'main',
+		resize: [ 500, 1000 ]
+	},
+	{
+		name: 'proj-1',
+		resize: [ 300, 700 ]
+	},
+	{
+		name: 'proj-2',
+		resize: [ 300, 700 ]
+	},
+	{
+		name: 'proj-3',
+		resize: [ 300, 700 ]
+	}
+];
+
+
+
+function buildImages() {
+	imageList.map(
+		image => 
+			image.resize.map(
+				size => gulp
+					.src(`${FILE_DIR}/${image.name}.jpg`)
+					.pipe(imageResize({ width: size }))
+					// .pipe(imageMin())
+					.pipe(rename(`${image.name}.min.${size}.jpg`))
+					.pipe(gulp.dest(BUILD_DIR))
+			)
+	);
+}
+
+gulp.task('build:images', buildImages);
+
+gulp.task('default', ['build:images']);
